c130b55ee5dc6cc5612e4dcb124eaa0b48b2cd49
Alexiseloza <47640237+Alexiseloza@users.noreply.github.com>
2019-10-05 17:25:00 -0300

    Admin pets

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+     return <div className="footer-header">
+				<p className="text-center mt-5">Powered by AlexisLoza - React Developer</p>
+				<p className=" text-white text-center">This demo includes React Hooks, Redux Hooks & LocalStorage.</p>
+				<p className="text-center  mt-5">Project Github --- <a href="https://github.com/Alexiseloza/Veterinary-Patients-ADMIN" className="text-white"> Go to Git </a>  </p>
+			</div>;
+}
+export default Footer;
